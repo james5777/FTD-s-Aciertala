79ffb425bf7a3e7f0d636d4400c06c036157c39b
james5777 <carojames79@gmail.com>
2025-10-16 08:37:45 -0500

    16/10

--- auto_descarga.py
+++ auto_descarga.py
@@ -18,7 +18,7 @@ def data_auto_descarga():
     # === 📅 CONFIGURACIÓN DE FECHAS ===
     # Puedes editar estas variables para ajustar el rango
     FECHA_INICIO = "2025-10-14 00:00"
-    FECHA_FIN =    "2025-10-14 23:59"
+    FECHA_FIN =    "2025-10-15 23:59"
 
     # Carpeta donde se descargará el CSV
     DOWNLOAD_DIR = os.path.join(os.getcwd(), "Archivos")
